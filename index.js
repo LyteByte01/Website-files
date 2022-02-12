2682af5816b3ed5fb0d7db51ae48d61d737109df
LyteByte01 <99282966+LyteByte01@users.noreply.github.com>
2022-02-12 22:45:10 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function printi() {
+	document.getElementById('topnav').style.position = "relative"
+	window.print()
+	document.getElementById('topnav').style.position = "fixed"
+	window.alert("I don't care if you wanted to save it as a pdf, why would you want to print a page???")
+}
